aa6f804d5a961ee4687d0b8b1551b3bb5cf978c6
mohsinkaleem <mohsin.kaleem512@gmail.com>
2020-01-10 14:55:28 +0530

    sd

--- DCube_lab/settings.py
+++ DCube_lab/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3#myojyyuy$a(volc*hhat7e#pk6bg&5)$vod7x!vt0gw%58ac'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,11 +128,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn DCube_lab.wsgi
